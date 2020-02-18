f3e1457aacfcd594e93e6743e46c4e8ab645d585
Korkia <korkija@gmail.com>
2020-02-18 15:37:34 +0200

    div

--- src/components/ItemWeather.js
+++ src/components/ItemWeather.js
@@ -23,6 +23,13 @@ const ItemWeather = (props) => {
                 <WindIcon speed={wind.speed}/>
                 <WindDirection deg={wind.deg}/>
             </div>
+            <div className='flex-container-left row'>
+                <img src={`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`} alt={weather[0].description}/>
+                {main.temp}
+                <WiCelsius size={34}/>
+                <WindIcon speed={wind.speed}/>
+                <WindDirection deg={wind.deg}/>
+            </div>
         </div>
     )
 };
